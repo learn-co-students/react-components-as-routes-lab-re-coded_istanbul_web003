8b0544503b195efbd5d9cdb5285a23cf4d20334b
bertacsevercan <bertac.severcan@gmail.com>
2020-09-21 13:35:02 +0300

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorList = () => {
+  return (
+    directors.map(director => 
+    <div key={director.name}>
+      <h3>Name: {director.name}</h3>
+      <p>Movies:</p>
+      <ul>
+        {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>)
+  )
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorList()}
     </div>
   );
 }
