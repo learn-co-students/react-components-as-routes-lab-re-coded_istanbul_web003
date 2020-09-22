0761ba3324c1f7ee7890e230e772c3d04aeaec2f
fadime94 <fadime.ozdemir11@gmail.com>
2020-09-22 13:04:23 +0300

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+
 const Movies = () => {
+  const displayMovies = ()=> {return movies.map(
+    movie=> (
+       <div>
+        <h2>{movie.title}</h2>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map(mov=><li>{mov}</li>)}
+        </ul>
+      </div>
+    )
+    )
+    
+  }
+  console.log("movie",movies)
   return (
+    
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+     {displayMovies()}
     </div>
   );
-};
-
+}
 export default Movies;
