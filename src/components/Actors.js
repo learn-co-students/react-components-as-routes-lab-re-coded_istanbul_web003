43ff300c85bef3c5d81d17e7768461ca4d595f0f
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-09-25 22:29:21 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  //console.log(actors)
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="actor">
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => <div key={index}>
+        {actor.name}
+        <ul>
+          {actor.movies.map((movie, index) =>
+            <li key={index}>
+              {movie}
+            </li>
+          )}
+        </ul>
+
+      </div>)}
     </div>
   );
 };
