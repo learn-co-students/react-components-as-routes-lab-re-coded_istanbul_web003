2886c42de59c12728048f50c432bb313738a79b8
ihab-sensei <ib-mashhadani@hotmail.com>
2020-10-06 12:54:42 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
+  const displayActors = () => {
+    return actors.map((actor) => (
+      <div>
+        <h2>{actor.name}</h2>
+        <ul>
+          {actor.movies.map((mov) => (
+            <li>{mov}</li>
+          ))}
+        </ul>
+      </div>
+    ));
+  };
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {displayActors()}
     </div>
   );
 };
