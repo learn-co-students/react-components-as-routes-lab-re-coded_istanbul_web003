5ae0f0d950e646e465bdd9ff4c7da474a7f0f19d
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-11-01 12:57:48 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,17 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>; 
+      {actors.map((actor) => (
+      	<div>
+      		<h3>Actor Name: {actor.name}</h3>
+      		<ul>
+      		{actor.movies.map(movie => (
+      			<li> {movie} </li>
+      			))}
+      		</ul>
+      	</div>
+      	))}
     </div>
   );
 };
